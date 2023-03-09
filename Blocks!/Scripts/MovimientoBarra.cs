87426fb884e23887e8bbf1ba4c3c52cd41b49f59
Ian007lol <127425177+Ian007lol@users.noreply.github.com>
2023-03-09 18:17:02 +0100

    Add files via upload

---
+++ Blocks!/Scripts/MovimientoBarra.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoBarra : MonoBehaviour
+{
+
+    public float velocidad = 0.1f;
+    public ElementoInteractivo pulsarizquierdo;
+    public ElementoInteractivo pulsarderecho;
+    float pulsado;
+    Vector3 posicionInicial;
+    // Start is called before the first frame update
+    void Start()
+    {
+        posicionInicial = transform.position;
+    }
+
+    public void Reset()
+    {
+        transform.position = posicionInicial;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pulsarizquierdo.pulsado)
+        {
+            pulsado = -1;
+        }
+        else if (pulsarderecho.pulsado)
+        {
+            pulsado = 1;
+        }
+        else
+        {
+            pulsado = Input.GetAxisRaw("Horizontal");
+        }
+       
+        float posX = transform.position.x + (pulsado * velocidad * Time.deltaTime);
+
+        transform.position = new Vector3(Mathf.Clamp(posX, -10, 10), transform.position.y, transform.position.z);
+    }
+   
+}
